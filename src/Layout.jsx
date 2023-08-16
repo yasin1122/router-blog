1c2517187ee02d90f19dada625ba6375c0f6e2f0
Yasin Cakal <whoisyasin@gmail.com>
2023-08-17 00:19:37 +0300

    first commit

---
+++ src/Layout.jsx
@@ -0,0 +1,20 @@
+import Footer from './Footer'
+import Header from './Header'
+import Nav from './Nav'
+import { Outlet } from 'react-router-dom'
+
+const Layout = ({ search, setSearch }) => {
+  return (
+    <div className='App'>
+      <Header title='React JS Blog' />
+      <Nav
+        search={search}
+        setSearch={setSearch}
+      />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
